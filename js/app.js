5f7dd610312abbe05eef1946b04409bc0664fd7f
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-02-17 14:39:26 +0100

    init repo

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/* 
+? TRACCIA:   
+  Attraverso l'apposita API di Boolean:
+    https://flynn.boolean.careers/exercises/api/random/mail
+  Generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+? BONUS:
+  Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
