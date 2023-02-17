809ce98ab443dfcacf7adb3b4ebbaa6a896028a3
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-02-17 15:15:41 +0100

    generate 10 random email + bonus 1

--- js/app.js
+++ js/app.js
@@ -7,3 +7,28 @@
 ? BONUS:
   Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
+
+// ? SVOLGIMENTO:
+
+const app = Vue.createApp({
+  data() {
+    return {
+      emailList: [],
+    };
+  },
+  methods: {
+    generateList() {
+      this.emailList = [];
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((res) => {
+            const result = res.data.response;
+            this.emailList.push(result);
+          });
+      }
+    },
+  },
+});
+
+app.mount("#app");
